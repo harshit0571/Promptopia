d75b7517be681fa151bef93e3328c2175b4c7065
harshit0571 <harshitchadha65@gmail.com>
2023-06-24 19:59:22 +0530

    setup done and homepage title and description made

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <section className="w-full flex justify-center items-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> Ai-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open source ai powered prompting tool to discover,
+        create and share ai prompts
+      </p>
+      {/* {feed} */}
+    </section>
+  );
+};
+
+export default Home;
